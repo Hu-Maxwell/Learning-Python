dc0a1af4454183ac656e1a75a5055e7e924ab6a2
dewe biag <dewebiag@gmail.com>
2024-10-01 22:03:42 -0700

    Added factorial_calculator

--- factorial_calculator.py
+++ factorial_calculator.py
@@ -3,7 +3,6 @@ def calculate_factorial(n):
         return 1
     return n * calculate_factorial(n-1)
 
-
 val = int(input("Input a number: "))
 result = calculate_factorial(val)
 print(f"Factorial: {result}")
